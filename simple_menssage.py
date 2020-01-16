9592ed5344718ae288e7ad2aef6886c5adf7835d
João Paulo Ramos Nogueira <jprnogueira@yahoo.com.br>
2020-01-16 17:59:34 -0300

    Delete simple_menssage.py

--- simple_menssage.py
+++
@@ -1,6 +0,0 @@
-# Pag. 55 - FAÇA VOCÊ MESMO
-
-# 2.1 - Mensagem simples: Armazenar uma mensagem em uma variável e, em seguida, exibir a mensagem.
-
-mensagem = "Exercício realizado em 16/01/2020"
-print(mensagem)
