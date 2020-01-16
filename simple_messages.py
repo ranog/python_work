5eeba25e98fa57353cafabefd0ed911d2e81a279
ranog <jprnogueira@yahoo.com.br>
2020-01-16 17:46:54 -0300

    Pag. 55 - FAÇA VOCÊ MESMO 2.2

---
+++ simple_messages.py
@@ -0,0 +1,9 @@
+# Pag. 55 - FAÇA VOCÊ MESMO
+
+# 2.2 - Mensagens simples: Armazenar uma mensagem em uma variável e, em seguida, exibir a mensagem. Então altere o valor de sua variável e mostre essa nova mensagem.
+
+mensagem = "Exercício realizado "
+print(mensagem)
+
+mensagem = "em 16/01/2020"
+print(mensagem)
