403cbfa52cd2478d2925bcec35ea468083c74d47
ranog <jprnogueira@yahoo.com.br>
2020-01-22 19:19:45 -0300

    Pag. 61 - Citação famosa - 2.5

---
+++ citacao_famosa.py
@@ -0,0 +1,10 @@
+# 2.4 - Citação famosa:
+# Encontre uma citação de uma pessoa famosa que você admire. Exiba
+# a citação e o nome do autor. Sua saída deverá ter a aparência a
+# seguir, incluindo as aspas: Albert Einstein certa vez disse: 
+# "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."
+
+pessoa = "Wesley D'Amico"
+citacao = "Há um mundo dentro de uma garrafa de vinho."
+
+print(pessoa + " certa vez disse:\n" + '"Há um mundo dentro de uma garrafa de vinho."')
