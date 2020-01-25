61401225e7a888d9c1526ff3e5e688e260ea01a0
ranog <jprnogueira@yahoo.com.br>
2020-01-25 07:25:39 -0300

    Pag. 74-75: Acrescentando elementos em uma lista

---
+++ capitulo_3/motorcycles_append.py
@@ -0,0 +1,16 @@
+# Concatenando elementos no final de uma lista
+
+motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
+
+motorcycles.append("ducati")
+print(motorcycles)
+
+# lista vazia
+motorcycles = []
+
+motorcycles.append("honda")
+motorcycles.append("yamaha")
+motorcycles.append("suzuki")
+
+print(motorcycles)
