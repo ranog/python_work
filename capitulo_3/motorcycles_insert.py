8d08b156e84acb6628eab13a58869e37dfbbbad2
ranog <jprnogueira@yahoo.com.br>
2020-01-26 15:41:58 -0300

    Pag. 78-79: Removendo um item de acordo com o valor

--- capitulo_3/motorcycles_insert.py
+++ capitulo_3/motorcycles_insert.py
@@ -1,4 +1,4 @@
-# - Pag. 75: Inserindo elementos em uma lista
+print("# - Pag. 75: Inserindo elementos em uma lista:")
 
 motorcycles = ["honda", "yamaha", "suzuki"]
 print(motorcycles)
@@ -6,13 +6,15 @@ print(motorcycles)
 motorcycles.insert(0, "ducati")
 print(motorcycles)
 
-# - Pag. 76: Removento elementos de uma lista
+
+print("\n# - Pag. 76: Removento elementos de uma lista:")
 
 # Usando a instrução del
 del motorcycles[0]
 print(motorcycles)
 
-# - Pag. 76-77: Removendo um item com o método pop()
+
+print("\n# - Pag. 76-77: Removendo um item com o método pop():")
 
 # O método pop() remove o último item de uma lista, mas permite que
 # trabalhe com esse item depois da remoção.
@@ -21,15 +23,40 @@ popped_motorcycles = motorcycles.pop()
 print(motorcycles)
 print(popped_motorcycles)
 
-# - Pag. 77-78: Removendo itens de qualquer posição em uma lista
+
+print("\n# - Pag. 77-78: Removendo itens de qualquer posição em uma lista:")
+# Modificando elementos de uma lista
 
 motorcycles = ["honda", "yamaha", "suzuki"]
+print(motorcycles)
 
 first_owned = motorcycles.pop(0)
 print("The first motorcycles I owned was a " + first_owned.title() + ".")
+print(motorcycles)
 
 # Quando quiser apagar um item de uma lista e esse item não vai ser 
 # usado de modo algum, utilize a instrução del; se quiser usar um 
 # item à medida que removê-lo, utilize o metodo pop()
 
-# - Pag. 78-79: Removendo um item de acordo com o valor
+print("\n# - Pag. 78-79: Removendo um item de acordo com o valor:")
+
+motorcycles = ["honda", "yamaha", "suzuki", "ducati"]
+print(motorcycles)
+
+motorcycles.remove("ducati")
+print(motorcycles)
+
+# Também podemos usar o método remove() para trabalhar com um valor 
+# que está sendo removido de uma lista
+
+motorcycles = ["honda", "yamaha", "suzuki", "ducati"]
+too_expensive = "ducati"
+
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
+
+# O método remove() apaga apenas a primeira ocorrência do valor. Se 
+# o valor aparecer mais de uma vez na lista, será necessário usar um
+# laço para determinar se todas as ocorrências desse valor foram 
+# removidas.
