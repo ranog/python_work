703dff50969a6d72c69d09e80302b9f318b695ec
ranog <jprnogueira@yahoo.com.br>
2020-01-28 23:16:57 -0300

    Pag. 79-80: FAÇA VOCÊ MESMO

---
+++ capitulo_3/lista_convidados.py
@@ -0,0 +1,25 @@
+
+#! /usr/bin/env python3
+
+"""
+    NOME
+        lista_convidados.py
+
+    SINOPSES
+        chmod +x lista_convidados.py
+        ./lista_convidados.py
+        
+    DESCRIÇÃO
+    
+        
+----------------------------------------------------------------------
+    
+    HISTÓRICO
+        20202801: João Paulo, janeiro de 2020
+        - Capítulo 3 / Introdução às listas
+            - FAÇA VOCÊ MESMO
+                - Pag. 79: 3.4
+                - Pag. 79: 3.5
+                - Pag. 79: 3.6
+                - Pag. 80: 3.7
+"""
