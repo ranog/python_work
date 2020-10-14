45b753896c109ecf8b93bf9ec40eb8df98a62185
ranog <jprnogueira@yahoo.com.br>
2020-10-14 13:18:52 -0300

    Percorrendo uma fatia com um laço - Pag. 100

--- capitulo_04-Trabalhando_com_listas/pg99_players.py
+++ capitulo_04-Trabalhando_com_listas/pg99_players.py
@@ -29,5 +29,5 @@ print(players[2:])
 print(players[-3:])
 
 print("\n- Here are the firt three players on my tean:")
-for player in players[:3]:
+for player in playerh:[:3]:
     print(player.title())
