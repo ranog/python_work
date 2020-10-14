29b98adf5da80bcb4f668fad33d3f1aa0c4b5746
ranog <jprnogueira@yahoo.com.br>
2020-10-14 13:00:53 -0300

    Fatiando uma lista - Pag. 100

--- capitulo_04-Trabalhando_com_listas/pg99_players.py
+++ capitulo_04-Trabalhando_com_listas/pg99_players.py
@@ -24,3 +24,6 @@ players = ['charles', 'martina', 'michael','florence','eli']
 print("\n- Lista: " + str(players))
 print(players[0:3])
 print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
