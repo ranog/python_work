c909e31e9126370a597613683a1429cafbf8cb32
ranog <jprnogueira@yahoo.com.br>
2020-10-16 22:52:49 -0300

    FAÇA VOCÊ MESMO - 4.13 – Buffet - Pag. 105

--- capitulo_04-Trabalhando_com_listas/pg105_faca_voce_mesmo.py
+++ capitulo_04-Trabalhando_com_listas/pg105_faca_voce_mesmo.py
@@ -9,10 +9,12 @@ SINOPSES
     ./pg105_faca_voce_mesmo.py
 
 DESCRIÇÃO
-    4.13 – Buffet: Um restaurante do tipo buffet oferece apenas cinco tipos básicos
-    de comida. Pense em cinco pratos simples e armazene-os em uma tupla.
+    4.13 – Buffet: Um restaurante do tipo buffet oferece apenas cinco tipos
+    básicos de comida. Pense em cinco pratos simples e armazene-os em uma
+    tupla.
     • Use um laço for para exibir cada prato oferecido pelo restaurante.
-    • Tente modificar um dos itens e cerifique-se de que Python rejeita a mudança.
+    • Tente modificar um dos itens e cerifique-se de que Python rejeita a
+    mudança.
     • O restaurante muda seu cardápio, substituindo dois dos itens com pratos
     diferentes. Acrescente um bloco de código que reescreva a tupla e, em
     seguida, use um laço for para exibir cada um dos itens do cardápio
@@ -26,7 +28,8 @@ HISTÓRICO
 
 """
 
-pratos = ('café colonial', 'comidas típicas das festas de são joão', 'carne de sol maracaípe', 'camarão havaiano maracaípe', 'torresmim')
+pratos = ('café colonial', 'comidas típicas das festas de são joão', 'carne\
+de sol maracaípe', 'camarão havaiano maracaípe', 'torresmim')
 
 print("4.13 – Buffet:")
 for prato in pratos:
@@ -37,7 +40,8 @@ for prato in pratos:
 
 print("\nTupla: " + str(pratos))
 
-pratos = ('café colonial', 'empadão', 'carne de sol maracaípe', 'dormência', 'torresmim')
+pratos = ('café colonial', 'empadão', 'carne de sol maracaípe', 'dormência',\
+'torresmim')
 
 print("\n4.13 – Buffet (revisado):")
 for prato in pratos:
