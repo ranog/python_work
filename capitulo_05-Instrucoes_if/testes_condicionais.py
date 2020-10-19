eae74f8229c1878f3cea6075701ac06187872b53
ranog <jprnogueira@yahoo.com.br>
2020-10-19 07:11:12 -0300

    5.2 – Mais testes condicionais - Pag. 117

--- capitulo_05-Instrucoes_if/testes_condicionais.py
+++ capitulo_05-Instrucoes_if/testes_condicionais.py
@@ -28,7 +28,7 @@ DESCRIÇÃO
     True e outros cinco avaliados como False.
 
     5.2 – Mais testes condicionais: Você não precisa limitar o número de testes
-    que criar em dez. Se quiser testar mais comparações, escreva outros testes
+    que criar em dez. Se quiser testar mais comparações, escreva outros teste5.2 – Mais testes condicionaiss
     e acrescente-os em conditional_tests.py. Tenha pelo menos um resultado True
     e um False para cada um dos casos a seguir:
 
@@ -86,3 +86,34 @@ print(young > 18)
 
 print("\n- Young > 18 or Old > 74? I predict False.")
 print(young > 18 or old > 74)
+
+print("\n5.2 – Mais testes condicionais:")
+
+print("\n- Testes de igualdade e de não igualdade com strings:")
+
+nome = 'Silva'
+sobrenome = 'silva'
+
+print("\nnome == Silva?")
+print(nome == 'Silva')
+
+print("\nnome == silva?")
+print(nome == 'silva')
+
+print("\n- Testes usando a função lower():")
+
+print("\n" + nome + " == " + sobrenome + " (sem a função lower())?")
+print(nome == sobrenome)
+
+print("\n" + nome + " == " + sobrenome + " (com a função lower())?")
+print(nome.lower() == sobrenome)
+
+print("\n- Testes numéricos que envolvam igualdade e não igualdade, maior e menor que, maior ou igual a e menor ou igual a:")
+
+answer = 42
+
+print("\nanswer == 42?")
+print(answer == 42)
+print("\nanswer != 42?")
+print(answer != 42)
+
