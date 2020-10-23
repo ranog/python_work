ff3c48975bfb6890193ad6c0ce192158b100984c
ranog <jprnogueira@yahoo.com.br>
2020-10-22 21:25:50 -0300

    Verificando se uma lista não está vazia - Pag. 125-126

--- capitulo_05-Instrucoes_if/toppings.py
+++ capitulo_05-Instrucoes_if/toppings.py
@@ -12,32 +12,44 @@ SINOPSES
     False
 
 DESCRIÇÃO
-    Compara o valor de requested_topping com o valor 'anchovies'. Se esses
-    dois valores não forem iguais, Python devolverá True e executará o
-    código após a instrução if. Se esses dois valores forem iguais, Python
-    devolverá  False e não executará o código após essa instrução.
+    Compara o valor de requested_topping com o valor 'anchovies'. Se esses dois
+    valores não forem iguais, Python devolverá True e executará o código após a
+    instrução if. Se esses dois valores forem iguais, Python devolverá  False e
+    não executará o código após essa instrução.
 
-    A palavra reservada in diz a Python para verificar a existência de
-    'mushrooms' e de 'pepperoni' na lista requested_toppings.
+    A palavra reservada in diz a Python para verificar a existência de 'mushrooms'
+    e de 'pepperoni' na lista requested_toppings.
 
     - Testando várias condições
-    Começamos com uma lista contendo os ingredientes solicitados. A
-    instrução if verifica se a pessoa pediu cogumelos ('mushrooms') em sua
-    pizza. Em caso afirmativo, uma mensagem será exibida para confirmar esse
-    ingrediente. O teste para pepperoni corresponde a outra instrução if
-    simples, e não a uma instrução elif ou else, portanto esse teste é executado
-    independentemente de o teste anterior ter passado ou não. 
-    O código verifica se queijo extra ('extra cheese') foi pedido, não
-    importando o resultado dos dois primeiros testes. Esses três testes 
-    independentes são realizados sempre que o programa é executado.
+
+    Começamos com uma lista contendo os ingredientes solicitados. A instrução
+    if verifica se a pessoa pediu cogumelos ('mushrooms') em sua pizza. Em caso
+    afirmativo, uma mensagem será exibida para confirmar esse ingrediente. O
+    teste para pepperoni corresponde a outra instrução if simples, e não a uma
+    instrução elif ou else, portanto esse teste é executado independentemente
+    de o teste anterior ter passado ou não. O código verifica se queijo extra
+    ('extra cheese') foi pedido, não importando o resultado dos dois primeiros
+    testes. Esses três testes independentes são realizados sempre que o programa
+    é executado.
 
 
-    Verificando itens especiais
+    - Verificando itens especiais
 
     O código verifica se a pessoa pediu pimentões verdes. Em caso afirmativo,
-    exibimos uma mensagem informando por que ela não pode ter pimentões
-    verdes. O bloco else garante que todos os demais ingredientes serão 
-    adicionados à pizza.
+    exibimos uma mensagem informando por que ela não pode ter pimentões verdes.
+    O bloco else garante que todos os demais ingredientes serão adicionados à
+    pizza.
+
+    - Verificando se uma lista não está vazia
+
+    Dessa vez, começamos com uma lista vazia de ingredientes. Em vez de passar
+    diretamente para um laço for, fazemos uma verificação rápida. Quando o nome
+    de uma lista é usado em uma instrução if, Python devolve True se a lista
+    contiver pelo menos um item; uma lista vazia é avaliada como False. Se
+    requested_toppings passar no teste condicional, executamos o mesmo laço for
+    do exemplo anterior. Se o teste condicional falhar, exibimos uma mensagem
+    perguntando ao cliente se ele realmente quer uma pizza simples, sem
+    ingredientes adicionais.
 
 ----------------------------------------------------------------------
 
@@ -47,8 +59,8 @@ HISTÓRICO
         iguais;
         - Verificando se um valor está em uma lista;
         - Testando várias condições - Pag. 121;
-        - Verificando itens especiais - Pag. 124-125
-
+        - Verificando itens especiais - Pag. 124-125;
+        - Verificando se uma lista não está vazia - Pag. 125-126;
 """
 
 
@@ -71,7 +83,20 @@ print("\nFinished making your pizza!")
 requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
 for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers': print("\n- Sorry, we are out of "+ requested_topping + " right now.")
+    if requested_topping == 'green peppers':
+        print("\n- Sorry, we are out of " + requested_topping + " right now.")
     else: print("\n- Adding " + requested_topping + ".")
 
-print("\nFinished making your pizza!") 
+print("\nFinished making your pizza!")
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping == 'green peppers':
+            print("\n- Sorry, we are out of " + requested_topping + " right now.")
+        else: print("\n- Adding " + requested_topping + ".")
+
+    print("\n- Finished making your pizza!")
+
+else:print("\n- Are you sure you want a plain pizza?")
