d3afbe4e63e6be2f3ab5239941bd4d9e1b6cebc9
ranog <jprnogueira@yahoo.com.br>
2020-10-30 17:30:36 -0300

    Usando continue em um laço (pg 161-162).

--- capitulo_07-Entrada_de_um_usuario_e_laco_while/counting.py
+++ capitulo_07-Entrada_de_um_usuario_e_laco_while/counting.py
@@ -7,13 +7,25 @@ NOME
 SINOPSES
     chmod +x counting.py
     ./counting.py
+
+    - Laço while em ação:
     1
     2
     3
     4
     5
 
+    - Usando continue em umwhile:
+    1
+    3
+    5
+    7
+    9
+
 DESCRIÇÃO
+
+    - Laço while em ação:
+
     Na primeira linha começamos a contar de 1 ao definir o valor de
     current_number com 1. O laço while é então configurado para
     continuar executando enquanto o valor de current_number for menor ou
@@ -27,15 +39,41 @@ DESCRIÇÃO
     sucessivamente. Quando o valor de current_number for maior que 5, o
     laço para de executar e o programa termina.
 
+    - Usando continue em um laço:
+
+    Inicialmente, definimos current_number com 0.
+    Como esse valor é menor que 10, Python entra no laço while. Uma vez
+    no laço, incrementamos o contador de 1, portanto current_number
+    passa a ser 1. A instrução if então verifica o módulo entre
+    current_number e 2.
+    Se o módulo for 0 (o que significa que current_number é divisível
+    por 2), a instrução continue diz a Python para ignorar o restante do
+    laço e voltar ao início. Se o número atual não for divisível por 2,
+    o restante do laço será executado e Python exibirá o número atual.
+
 HISTÓRICO
     20202910: João Paulo, outubro de 2020.
         - Laço while em ação (pg 157).
 
+        - Usando continue em um laço (pg 161-162).
+
 """
 
-
+print("\n- Laço while em ação: ")
 current_number = 1
 
 while current_number <= 5:
     print(current_number)
     current_number += 1
+
+print("\n- Usando continue em um laço: ")
+
+current_number = 0
+
+while current_number < 10:
+    current_number += 1
+
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
