30f6faaf684389b6670308849a55a3d5bec80632
ranog <jprnogueira@yahoo.com.br>
2020-11-02 17:31:12 -0300

    8.1 - Mensagem (pg 171)

---
+++ capitulo_08-Funcoes/mensagem.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+"""
+NOME
+    mensagem.py - FAÇA VOCÊ MESMO.
+
+SINOPSES
+    chmod +x mensagem.py
+    ./mensagem.py
+
+    - Estou aprendendo um pouco sobre funções!!!
+
+DESCRIÇÃO
+    8.1 – Mensagem: Escreva uma função chamada display_message() que
+    mostre uma frase informando a todos o que você está aprendendo neste
+    capítulo. Chame a função e certifique-se de que a mensagem seja
+    exibida corretamente.
+
+HISTÓRICO
+    20200211: João Paulo, outubro de 2020.
+        - 8.1 - Mensagem (pg 171).
+
+"""
+
+
+def display_message():
+    print("\n- Estou aprendendo um pouco sobre funções!!!")
+
+display_message()
