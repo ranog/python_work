d2bbb930dc286cdcaf0551838a2c7169360c2b5b
ranog <jprnogueira@yahoo.com.br>
2020-11-02 17:58:47 -0300

    Várias chamadas de função (pg 172).

--- capitulo_08-Funcoes/pets.py
+++ capitulo_08-Funcoes/pets.py
@@ -9,6 +9,8 @@ SINOPSES
     ./pets.py
 
 DESCRIÇÃO
+    - Argumentos posicionais:
+
     A definição mostra que essa função precisa de um tipo de animal e de
     seu nome. Quando chamamos describe_pet(), devemos fornecer o tipo do
     animal e um nome, nessa ordem. Por exemplo, na chamada da função, o
@@ -17,9 +19,19 @@ DESCRIÇÃO
     função, esses dois parâmetros são usados para exibir informações
     sobre o animal de estimação descrito.
 
+    - Várias chamadas de função:
+
+    Nessa segunda chamada da função, passamos os argumentos 'dog' e
+    'willie' a describe_pet(). Assim como no conjunto anterior de
+    argumentos que usamos, Python faz a correspondência entre 'dog' e o
+    parâmetro animal_type e entre 'willie' e o parâmetro pet_name. Como
+    antes, a função faz sua tarefa, porém, dessa vez, exibe valores para
+    um cachorro chamado Willie.
+
 HISTÓRICO
     20200211: João Paulo, outubro de 2020.
         - Argumentos posicionais (pg 172).
+        - Várias chamadas de função (pg 172).
 
 """
 
@@ -30,3 +42,4 @@ def describe_pet(animal_type, pet_name):
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
 describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
