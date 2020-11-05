bf543084c169b15bbf2d46e433d5c1e1a3b98745
ranog <jprnogueira@yahoo.com.br>
2020-11-05 13:31:21 -0300

    Modificando uma lista em uma função (pg 183-186).

---
+++ capitulo_08-Funcoes/printing_models.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+"""
+NOME
+    printing_models.py - Modificando uma lista em uma função.
+
+SINOPSES
+    chmod +x printing_models.py
+    ./printing_models.py
+
+DESCRIÇÃO
+
+HISTÓRICO
+    20200511: João Paulo, outubro de 2020.
+        - Modificando uma lista em uma função (pg 183-186).
+
+"""
+
+
+# Começa com alguns designs que devem ser impressos 
+unprinted_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
