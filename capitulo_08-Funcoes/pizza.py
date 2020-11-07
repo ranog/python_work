e0d5cf6d65021851883eb14adca75ddd74f2dfbe
ranog <jprnogueira@yahoo.com.br>
2020-11-07 17:29:12 -0300

    Misturando argumentos posicionais e arbitrários (pg 188).

--- capitulo_08-Funcoes/pizza.py
+++ capitulo_08-Funcoes/pizza.py
@@ -7,14 +7,32 @@ NOME
 SINOPSES
     chmod +x pizza.py
     ./pizza.py
-    #Parte comentada na função
-    ('pepperoni',) 
+
+    ## Parte comentada na função ##
+
+    ('pepperoni',)
     ('mushrooms', 'green peppers', 'extra cheese')
+
+------------------------------------------------------------------------
+
+    ## Passando um número arbitrário de argumentos. ##
 
     Making a pizza with the following toppings:
     - pepperoni
 
     Making a pizza with the following toppings:
+    - mushrooms
+    - green peppers
+    - extra cheese
+
+------------------------------------------------------------------------
+
+    ## Misturando argumentos posicionais e arbitrários. ##
+
+    Making a 16-inch pizza with the following toppings:
+    - pepperoni
+
+    Making a 12-inch pizza with the following toppings:
     - mushrooms
     - green peppers
     - extra cheese
@@ -31,23 +49,38 @@ DESCRIÇÃO
     A função responde de forma apropriada, independentemente de receber
     um ou três valores.
 
+    - Misturando argumentos posicionais e arbitrários
+
+    O parâmetro que aceita um número arbitrário de argumentos deve ser
+    colocado por último na definição da função. Python faz a
+    correspondência de argumentos posicionais e nomeados antes, e depois
+    agrupa qualquer argumento remanescente no último parâmetro.
+
+    Na definição da função, Python armazena o primeiro valor recebido no
+    parâmetro size. Todos os demais valores que vierem depois são
+    armazenados na tupla toppings. As chamadas da função incluem um
+    argumento para o tamanho antes, seguido de tantos ingredientes
+    quantos forem necessários.
+
 HISTÓRICO
     20200711: João Paulo, outubro de 2020.
         - Passando um número arbitrário de argumentos. (pg 187-188).
+        - Misturando argumentos posicionais e arbitrários (pg 188).
 
 """
 
 
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Exibe a lista de ingredientes pedidos."""
     #print(toppings)
 
     """Apresenta a pizza que estamos prestes a preparar."""
-    print("\nMaking a pizza with the following toppings: ")
+    print("\nMaking a " + str(size) +
+          "-inch pizza with the following toppings: ")
 
     for topping in toppings:
         print("- " + topping)
 
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
