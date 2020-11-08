2ed3fddf648095d7f4e70d962e854ea04d18c318
ranog <jprnogueira@yahoo.com.br>
2020-11-08 15:18:00 -0300

    Importando um módulo completo (pg 191-192).

---
+++ capitulo_08-Funcoes/making_pizzas.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+
+"""
+NOME
+    making_pizzas.py - Importando ummódulo completo.
+
+SINOPSES
+    chmod +x making_pizzas.py
+    ./making_pizzas.py
+
+    Making a 16-inch pizza with the following toppings:
+    - pepperoni
+
+    Making a 12-inch pizza with the following toppings:
+    - mushrooms
+    - green peppers
+    - extra cheese
+
+DESCRIÇÃO
+    Um módulo é um arquivo terminado em .py que contém o código que
+    queremos importar para o nosso programa. Vamos criar um módulo que
+    contenha a função make_pizza().
+    Para criar esse módulo, removeremos tudo que está no arquivo
+    pizza.py, exceto a função make_pizza().
+
+    Agora criaremos um arquivo separado chamado making_pizzas.py no
+    mesmo diretório em que está pizza.py. Esse arquivo importa o módulo
+    que acabamos de criar e, em seguida, faz duas chamadas para make_pizza().
+
+    Quando Python lê esse arquivo, a linha import pizza lhe diz para
+    abrir o arquivo pizza.py e copiar todas as funções dele para esse
+    programa. Você não vê realmente o código sendo copiado entre os
+    arquivos porque Python faz isso internamente, à medida que o
+    programa executa. Tudo que você precisa saber é que qualquer função
+    definida em pizza.py agora estará disponível em making_pizzas.py.
+
+HISTÓRICO
+    20200811: João Paulo, novembro de 2020.
+        - Importando um módulo completo (pg 191-192).
+
+"""
+
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
--- capitulo_08-Funcoes/pizza.py
+++ capitulo_08-Funcoes/pizza.py
@@ -5,82 +5,30 @@ NOME
     pizza.py - Passando um número arbitrário de argumentos.
 
 SINOPSES
-    chmod +x pizza.py
-    ./pizza.py
-
-    ## Parte comentada na função ##
-
-    ('pepperoni',)
-    ('mushrooms', 'green peppers', 'extra cheese')
-
-------------------------------------------------------------------------
-
-    ## Passando um número arbitrário de argumentos. ##
-
-    Making a pizza with the following toppings:
-    - pepperoni
-
-    Making a pizza with the following toppings:
-    - mushrooms
-    - green peppers
-    - extra cheese
-
-------------------------------------------------------------------------
-
-    ## Misturando argumentos posicionais e arbitrários. ##
-
-    Making a 16-inch pizza with the following toppings:
-    - pepperoni
-
-    Making a 12-inch pizza with the following toppings:
-    - mushrooms
-    - green peppers
-    - extra cheese
+    nome_do_módulo.nome_da_função()
 
 DESCRIÇÃO
-    O asterisco no nome do parâmetro *toppings diz a Python para criar
-    uma tupla vazia chamada toppings e reunir os valores recebidos nessa
-    tupla. A instrução print no corpo da função gera uma saída que
-    mostra que Python é capaz de tratar uma chamada de função com um
-    valor e outra chamada com três valores. As chamadas são tratadas de
-    modo semelhante. Observe que Python agrupa os argumentos em uma
-    tupla, mesmo que a função receba apenas um valor.
-
-    A função responde de forma apropriada, independentemente de receber
-    um ou três valores.
-
-    - Misturando argumentos posicionais e arbitrários
-
-    O parâmetro que aceita um número arbitrário de argumentos deve ser
-    colocado por último na definição da função. Python faz a
-    correspondência de argumentos posicionais e nomeados antes, e depois
-    agrupa qualquer argumento remanescente no último parâmetro.
-
-    Na definição da função, Python armazena o primeiro valor recebido no
-    parâmetro size. Todos os demais valores que vierem depois são
-    armazenados na tupla toppings. As chamadas da função incluem um
-    argumento para o tamanho antes, seguido de tantos ingredientes
-    quantos forem necessários.
+    Um módulo é um arquivo terminado em .py que contém o código que
+    queremos importar para o nosso programa. Vamos criar um módulo que
+    contenha a função make_pizza().
+    Para criar esse módulo, removeremos tudo que está no arquivo
+    pizza.py, exceto a função make_pizza().
 
 HISTÓRICO
     20200711: João Paulo, outubro de 2020.
         - Passando um número arbitrário de argumentos. (pg 187-188).
         - Misturando argumentos posicionais e arbitrários (pg 188).
+    
+    20200811: João Paulo, novembro de 2020.
+        - Importando ummódulo completo (pg 191-192).
 
 """
 
 
 def make_pizza(size, *toppings):
-    """Exibe a lista de ingredientes pedidos."""
-    #print(toppings)
-
     """Apresenta a pizza que estamos prestes a preparar."""
     print("\nMaking a " + str(size) +
           "-inch pizza with the following toppings: ")
 
     for topping in toppings:
         print("- " + topping)
-
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
