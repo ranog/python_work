28919fbd995b939f9a415115db4b20e890be340a
ranog <jprnogueira@yahoo.com.br>
2020-11-14 22:55:20 -0300

    Definindo atributos e métodos da classe-filha (pg 212-213).

--- capitulo_09-Classes/electric_car.py
+++ capitulo_09-Classes/electric_car.py
@@ -8,6 +8,7 @@ SINOPSES
     chmod +x electric_car.py
     ./electric_car.py
     2016 Tesla Model S
+    This car has a 70-kWh battery.
 
 DESCRIÇÃO
     Começamos com Car. Quando criamos uma classe-filha, a classe-pai
@@ -37,9 +38,28 @@ DESCRIÇÃO
     Car, portanto podemos agora começar a definir atributos e métodos
     específicos aos carros elétricos.
 
+    - Definindo atributos e métodos da classe-filha:
+
+    Adicionamos um novo atributo self.battery_size e definimos seu valor
+    inicial, por exemplo, com 70. Esse atributo será associado a todas
+    as instâncias criadas a partir da classe ElectricCar, mas não será
+    associado a nenhuma instância de Car. Também adicionamos um método
+    chamado describe_battery(), que exibe informações sobre a bateria.
+    Quando chamamos esse método, temos uma descrição que é claramente
+    específica de um carro elétrico.
+
+    Um atributo ou método que possa pertencer a qualquer carro, isto é,
+    que não seja específico de um carro elétrico, deve ser adicionado à
+    classe Car , em vez de ser colocado na classe ElectricCar.
+    Então qualquer pessoa que usar a classe Car terá essa funcionalidade
+    disponível também, e a classe ElectricCar conterá apenas códigos
+    para as informações e comportamentos específicos de veículos
+    elétricos.
+
 HISTÓRICO
     20201411: João Paulo, novembro de 2020.
         - Método __init__() de uma classe-filha (pg 211-212).
+        - Definindo atributos e métodos da classe-filha (pg 212-213).
 
 """
 
@@ -55,11 +75,13 @@ class Car():
 
         self.odometer_reading = 0
 
+
     def get_descriptive_name(self):
         """Devolve um nome descritivo, formatado de modo elegante."""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
 
         return long_name.title()
+
 
     def read_odometer(self):
         """Exibe uma frase que mostra a milhagem do carro."""
@@ -88,10 +110,20 @@ class ElectricCar(Car):
     """Representa aspectos específicos de veículos elétricos.
     """
     def __init__(self, make, model, year):
-        """Inicializa os atributos da classe-pai.
+        """Inicializa os atributos da classe-pai. Em seguida, inicializa
+        os atributos específicos de um carro elétrico.
         """
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+
+    def describe_battery(self):
+        """Exibe uma frase que descreve a capacidade da bateria.
+        """
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
