b5d6a1f6b0ea22a4570b809c51f3079da9004cea
ranog <jprnogueira@yahoo.com.br>
2020-11-16 23:35:16 -0300

    Armazenando várias classes em um módulo (pg 220-221).

--- capitulo_09-Classes/car.py
+++ capitulo_09-Classes/car.py
@@ -82,11 +82,13 @@ HISTÓRICO
 
     20201611: João Paulo, novembro de 2020.
         - Importando uma única classe (pg 218-219).
+        - Armazenando várias classesem um módulo (pg 220-221).
 
 """
 
 
-"""Uma classe que pode ser usada para representar um carro.
+"""Um conjunto de classes usado para representar carros à gasolina e
+elétricos.
 """
 class Car():
     """Uma tentativa simples de representar um carro."""
@@ -99,11 +101,13 @@ class Car():
 
         self.odometer_reading = 0
 
+
     def get_descriptive_name(self):
         """Devolve um nome descritivo, formatado de modo elegante."""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
 
         return long_name.title()
+
 
     def read_odometer(self):
         """Exibe uma frase que mostra a milhagem do carro."""
@@ -126,3 +130,68 @@ class Car():
         """Soma a quantidade especificada ao valor de leitura do
         hodômetro."""
         self.odometer_reading += miles
+
+
+    def fill_gas_tank(self):
+        print("Tank capacity is 290 L.")
+
+
+class Battery():
+    """Uma tentativa simples de modelar uma bateria para um carro elétrico.
+    """
+    def __init__(self, battery_size=70):
+        """Inicializa os atributos da bateria.
+        """
+        self.battery_size = battery_size
+
+
+    def describe_battery(self):
+        """Exibe uma frase que descreve a capacidade da bateria.
+        """
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+    def get_range(self):
+        """Exibe uma frase sobre a distância que o carro é capaz de
+        percorrer com essa bateria.
+        """
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range) 
+        message += " miles on a full charge."
+
+        print(message)
+
+
+    def upgrade_battery(self):
+        """Verificar a capacidade da bateria e defini com 85 se o valor
+        for diferente.
+        """
+        if self.battery_size != 85:
+            self.battery_size = 85
+        
+
+class ElectricCar(Car):
+    """Representa aspectos específicos de veículos elétricos.
+    """
+    def __init__(self, make, model, year):
+        """Inicializa os atributos da classe-pai. Em seguida, inicializa
+        os atributos específicos de um carro elétrico.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+
+    def describe_battery(self):
+        """Exibe uma frase que descreve a capacidade da bateria.
+        """
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+    def fill_gas_tank(self):
+        """Carros elétricos não têm tanques de gasolina.
+        """
+        print("This car doesn't need a gas tank!")
---
+++ capitulo_09-Classes/my_electric_car.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+"""
+NOME
+    my_electric_car.py - Armazenando várias classes em um módulo.
+
+SINOPSES
+    chmod +x my_electric_car.py
+    ./my_electric_car.py
+    2016 Tesla Model S
+    This car has a 70-kWh battery.
+    This car can go approximately 240 miles on a full charge.
+    This car doesn't need a gas tank!
+
+    This car has a 85-kWh battery.
+    This car can go approximately 270 miles on a full charge.
+
+    1990 Lamborghini Lm002
+    Tank capacity is 290 L.
+
+DESCRIÇÃO
+    Esse código gera a mesma saída que vimos antes, embora a maior parte
+    da lógica esteja oculta em um módulo.
+
+HISTÓRICO
+    20201611: João Paulo, novembro de 2020.
+        - Armazenando várias classes em um módulo (pg 220-221).
+
+"""
+
+
+from car import ElectricCar 
+from car import Car
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.fill_gas_tank()
+
+print() 
+
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+
+print()
+
+my_car = Car('lamborghini', 'LM002', 1990)
+print(my_car.get_descriptive_name())
+my_car.fill_gas_tank()
