11072d393ca539b203ea6a71507a655fceaa6794
ranog <jprnogueira@yahoo.com.br>
2020-11-18 13:14:24 -0300

    9.13 – Reescrevendo o programa com OrderedDict (pg 225).

--- capitulo_09-Classes/glossario.py
+++ capitulo_09-Classes/glossario.py
@@ -8,6 +8,44 @@ SINOPSES
     chmod +x glossario.py
     ./glossario.py
 
+    range():
+    - Facilita gerar uma série de números.
+
+    len():
+    - Retorna o tamanho de uma lista em Python.
+
+    lower():
+    - ('Ignora as diferenças entre letras maiúsculas e minúsculas ao
+    verificar a igualdade.',)
+
+    lista comprehension:
+    - Uma list comprehension combina o laço for e a criação de novos
+    elementos em uma linha, e concatena cada novo elemento
+    automaticamente (Ex: squares = [value**2 for value in range(1,11)])..
+
+    [:]:
+    - Para copiar uma lista, podemos criar uma fatia que inclua a lista
+    original inteira omitindo o primeiro e o segundo índices ([:]). Isso
+    diz a Python para criar uma lista que começa no primeiro item e
+    termina no último, gerando uma cópia da lista toda.
+
+    title():
+    - Exibe cada palavra com uma letra maiúscula no início.
+
+    upper():
+    - Pode mudar uma string para que tenha somente letras  maiúsculas.
+
+    rstrip():
+    - Garante que não haja espaços em branco do lado direito de uma
+    string.
+
+    str():
+    - Essa função diz a Python para representar valores que não  são
+    strings como strings.
+
+    sorted():
+    - Permite exibir sua lista em uma ordem em particular, mas não afeta
+    a ordem propriamente dita da lista.
 
 DESCRIÇÃO
     9.13 – Reescrevendo o programa com OrderedDict: Comece com o
@@ -33,13 +71,18 @@ glossario = OrderedDict()
 glossario['range'] = 'Facilita gerar uma série de números.'
 glossario['len'] = 'Retorna o tamanho de uma lista em Python.'
 glossario['lower'] = 'Ignora as diferenças entre letras maiúsculas e minúsculas ao verificar a igualdade.',
-glossario['comprehension'] = 'Uma list comprehension combina o laço for e a criação de novos elementos em uma linha, e concatena cada novo elemento automaticglossa (Ex: squares = [value**2 for value in range(1,11)]).'
-glossario['[:]'] = 'Para copiar uma lista, podemos criar uma fatia que incliglossario original inteira omitindo o primeiro e o segundo índices ( [:] ). Isso diz a Python para criar uma lista que começa no primeiro item e termina no último, gerando uma cópia da lista toda.'
+glossario['comprehension'] = 'Uma list comprehension combina o laço for e a \
+criação de novos elementos em uma linha, e concatena cada novo elemento\
+ automaticamente (Ex: squares = [value**2 for value in range(1,11)])..'
+glossario['[:]'] = 'Para copiar uma lista, podemos criar uma fatia que inclua \
+a lista original inteira omitindo o primeiro e o segundo índices ([:]). Isso \
+diz a Python para criar uma lista que começa no primeiro item e termina no \
+último, gerando uma cópia da lista toda.'
 glossario['title'] = 'Exibe cada palavra com uma letra maiúscula no início.'
 glossario['upper'] = 'Pode mudar uma string para que tenha somente letras  maiúsculas.'
-glossario['rstrip'] = 'Garante que não haja espaços em branco do ladoglossari de uma string.'
-glossario['str'] = 'Essa função diz a Python para representar valores que não  são strings como strings.'
-
+glossario['rstrip'] = 'Garante que não haja espaços em branco do lado direito de uma string.'
+glossario['str'] = 'Essa função diz a Python para representar valores que não \
+são strings como strings.'
 glossario['sorted'] = 'Permite exibir sua lista em uma ordem em particular, mas não afeta a ordem propriamente dita da lista.'
 
 for word, meaning in glossario.items():
