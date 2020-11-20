098202e255f5f4a785b858f5de92f28a09f2a855
ranog <jprnogueira@yahoo.com.br>
2020-11-20 17:35:47 -0300

    Seu aniversário está contido em pi (pg 236-237)?

--- capitulo_10-Arquivos_e_excecoes/pi_string.py
+++ capitulo_10-Arquivos_e_excecoes/pi_string.py
@@ -19,15 +19,25 @@ DESCRIÇÃO
         3.14159265358979323846264338327950288419716939937510...
         1000002
         
+        Seu aniversário está contido em pi (pg 236-237)?
+
+        Enter your birthday, in the form mmddyy: 130820
+
+        - Your birthday appears in the first million digits of pi!
+
         Python não tem nenhum limite inerente para a quantidade de dados
         com que podemos trabalhar; podemos trabalhar com tantos dados
         quantos a memória de seu sistema for capaz de tratar.
+
+
+    - Seu aniversário está contido em pi (pg 236-237)?:
 
 ------------------------------------------------------------------------
 
 HISTÓRICO
     20202011: João Paulo, novembro de 2020.
         - Arquivos grandes: um milhão de dígitos (pg 235-236).
+        - Seu aniversário está contido em pi (pg 236-237)?
 
 """
 
@@ -44,3 +54,13 @@ for line in lines:
 
 print(pi_string[:52] + "...")
 print(len(pi_string))
+
+print("\nSeu aniversário está contido em pi (pg 236-237)?")
+
+birthday = input("\nEnter your birthday, in the form mmddyy: ")
+
+if birthday in pi_string:
+    print("\n- Your birthday appears in the first million digits of pi!")
+
+else:
+    print("\n- Your birthday does not appear in the first million digits of pi.")
