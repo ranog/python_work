61bad78c6d3a3af90b87f7dad0e9f072385e2b23
ranog <jprnogueira@yahoo.com.br>
2020-11-22 18:06:11 -0300

    - Usando exceções para evitar falhas (pg 241-242).

--- capitulo_10-Arquivos_e_excecoes/division.py
+++ capitulo_10-Arquivos_e_excecoes/division.py
@@ -10,6 +10,19 @@ SINOPSES
 
     You can't divide by zero!
 
+    Give me two numbers, and I'll divide them.
+    Enter 'q' to quit.
+
+    First number: 5
+
+    Second number: 0
+    Traceback (most recent call last):
+    File "./division.py", line 86, in <module>
+        answer = int(first_number) / int(second_number)
+    ZeroDivisionError: division by zero
+
+    o shell devolveu 1
+
 ------------------------------------------------------------------------
 
 DESCRIÇÃO
@@ -35,12 +48,32 @@ DESCRIÇÃO
         interpretador procurará um bloco except cujo erro coincida com
         aquele levantado e executará o código desse bloco.
 
+
+    - Usando exceções para evitar falhas:
+        Esse programa pede que o usuário forneça um primeiro número
+        (first_number) e, se o usuário não digitar q para sair, pede um
+        segundo número (second_number). Então dividimos esses dois
+        números para obter uma resposta (answer). O programa não faz
+        nada para tratar erros, portanto pedir que uma divisão por zero
+        seja feita causará uma falha no programa.
+
+        O fato de o programa falhar é ruim, mas também não é uma boa
+        ideia deixar que os usuários vejam os tracebacks. Usuários que
+        não sejam técnicos ficarão confusos com eles e, em um ambiente
+        malicioso, invasores aprenderão mais do que você quer que eles
+        saibam a partir de um traceback. Por exemplo, eles saberão o
+        nome de seu arquivo de programa e verão uma parte de seu código
+        que não está funcionando de forma apropriada. Às vezes, um
+        invasor habilidoso pode usar essas informações para determinar
+        os tipos de ataque que podem usar contra o seu código.
+
 ------------------------------------------------------------------------
 
 HISTÓRICO
     20202111: João Paulo, novembro de 2020.
         - Tratando a exceção ZeroDivisionError (pg 240).
         - Usando blocos try-except (pg 241).
+        - Usando exceções para evitar falhas (pg 241-242).
 
 """
 
@@ -50,3 +83,18 @@ try:
 
 except ZeroDivisionError:
     print("\nYou can't divide by zero!")
+
+print("\nGive me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number.lower() == 'q':
+        break
+
+    second_number = input("\nSecond number: ")
+    if second_number.lower() == 'q':
+        break
+
+    answer = int(first_number) / int(second_number)
+    print(answer)
