8b51093d3b952156e3a890bd559b6cb8f5db3c60
ranog <jprnogueira@yahoo.com.br>
2020-11-24 07:34:21 -0300

    10.7 – Calculadora para adição (pg 248).

--- capitulo_10-Arquivos_e_excecoes/sum.py
+++ capitulo_10-Arquivos_e_excecoes/sum.py
@@ -7,7 +7,19 @@ NOME
 SINOPSES
     chmod +x sum.py
     ./sum.py
+    
+    - Entre com 'q' para sair.
+    Entre com o 1° número: a
+    Entre com o 2° número: 1
+    - Não podemos realizar essa soma no momento, volte mais tarde. ;)
 
+    - Entre com 'q' para sair.
+    Entre com o 1° número: 1
+    Entre com o 2° número: 2
+    1 + 2 = 3.0
+
+    - Entre com 'q' para sair.
+    Entre com o 1° número: q
 
 ------------------------------------------------------------------------
 
@@ -21,12 +33,18 @@ DESCRIÇÃO
         de entrada não seja um número e apresente uma mensagem de erro
         simpática. Teste seu programa fornecendo dois números e, em
         seguida, digite um texto no lugar de um número.
+    
+    10.7 – Calculadora para adição:         Coloque o código do
+        Exercício 10.6 em um laço while para que o usuário possa
+        continuar fornecendo números, mesmo se cometerem um erro e
+        digitarem um texto no lugar de um número.
 
 ------------------------------------------------------------------------
 
 HISTÓRICO
     20202411: João Paulo, novembro de 2020.
         - 10.6 - Adição (pg 248).
+        - 10.7 – Calculadora para adição (pg 248).
 
 """
 
@@ -44,6 +62,7 @@ def soma(x, y):
 
 while True:
     print("\n- Entre com 'q' para sair.")
+    
     x = input("Entre com o 1° número: ")
     if x.lower() == 'q':
         break
@@ -52,3 +71,4 @@ while True:
     if y.lower() == 'q':
         break
 
+    soma(x, y)
