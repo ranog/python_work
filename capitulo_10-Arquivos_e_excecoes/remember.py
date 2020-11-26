16b848179b863e32a6c2e3449fae463aae5f4d86
ranog <jprnogueira@yahoo.com.br>
2020-11-26 07:24:36 -0300

    Refatoração (pg 252-254).

--- capitulo_10-Arquivos_e_excecoes/remember.py
+++ capitulo_10-Arquivos_e_excecoes/remember.py
@@ -34,11 +34,38 @@ DESCRIÇÃO
 
         Essa é a saída que você verá se o programa já foi executado pelo
         menos uma vez.
+
+    - Refatoração
+
+         A nova função get_stored_username() tem um propósito claro,
+         conforme informado pela docstring. Essa função recupera um nome
+         de usuário já armazenado e devolve esse nome se encontrar um.
+         Se o arquivo username.json não existir, a função devolverá
+         None. Essa é uma boa prática: uma função deve devolver o valor
+         esperado ou None. Isso nos permite fazer um teste simples com o
+         valor de retorno da função. Exibimos uma mensaguserE
+         boas-vindas de volta ao usuário se a tentuserE de recuperar um
+         nome foi bem-sucedida; caso contrário, pedimos que um novo nome
+         de usuário seja fornecido.
+
+        Cada função nessa última versão de remember.py tem um propósito
+        único e claro. Chamamos greet_user() e essa função exibe uma
+        mensagem apropriada: ela dá as boas-vindas de volta a um usuário
+        existente ou saúda um novo usuário. Isso é feito por meio da
+        chamada a get_stored_username(), que é responsável somente por
+        recuperar um nome de usuário já armazenado, caso exista. Por
+        fim, greet_user() chama get_new_username() se for necessário;
+        essa função é responsável somente por obter um novo nome de
+        usuário e armazená-lo. Essa separação do trabalho em
+        compartimentos é uma parte essencial na escrita de um código
+        claro, que seja fácil de manter e de estender.
+
 ------------------------------------------------------------------------
 
 HISTÓRICO
     20202511: João Paulo, novembro de 2020.
         - Salvando e lendo dados gerados pelo usuário (pg 250-252).
+        - Refatoração (pg 252-254).
 
 """
 
@@ -46,25 +73,44 @@ HISTÓRICO
 
 import json
 
-# Carrega o nome do usuário se foi armazenado anteriormente. Caso
-# contrário, pede que o usuário forneça o nome e armazena essa
-# informação.
 
-filename = 'username.json'
+def get_stored_username():
+    """Obtém o nome do usuário já armazenado se estiver disponível."""
 
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
+    filename = 'username.json'
 
-except FileNotFoundError:
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """Pede um novo nome de usuário."""
     username = input("- What is your name? ")
+    filename = 'username.json'
 
     with open(filename, 'w') as f_obj:
         json.dump(username, f_obj)
 
+    return username
 
-    print("We'll remember you when you come back, " +
+
+def greet_user():
+    """Saúda o usuário pelo nome."""
+    username = get_stored_username() 
+    if username:
+        print("- Welcome back, " + username.title() + "!")
+
+    else:
+        username = get_new_username()
+
+        print("We'll remember you when you come back, " +
     username.title() + "!")
 
-else:
-    print("- Welcome back, " + username.title() + "!")
+
+greet_user()
