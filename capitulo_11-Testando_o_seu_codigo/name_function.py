816cde36b8768d89f0ecf7aace2e675256dfe3a2
ranog <jprnogueira@yahoo.com.br>
2020-11-27 17:23:19 -0300

    Respondendo a um teste que falhou (pg 261-262).

--- capitulo_11-Testando_o_seu_codigo/name_function.py
+++ capitulo_11-Testando_o_seu_codigo/name_function.py
@@ -15,6 +15,12 @@ DESCRIÇÃO
     converte as primeiras letras do nome para maiúsculas e devolve o
     nome completo.
 
+    Nessa nova versão de get_formatted_name(), o nome do meio é
+    opcional. Se um nome do meio for passado para a função (if middle:),
+    o nome completo conterá um primeiro nome, um nome do meio e um
+    sobrenome. Caso contrário, o nome completo será constituído apenas
+    de um primeiro nome e de um sobrenome.
+
 ------------------------------------------------------------------------
 
 HISTÓRICO
@@ -23,14 +29,19 @@ HISTÓRICO
 
     20202711: João Paulo, novembro de 2020.
         - Um teste que falha (pg 260-261).
+        - Respondendo a um teste que falhou (pg 261-262)
 
 
 """
 
 
-def get_formatted_name(first, middle, last):
+def get_formatted_name(first, last, middle=''):
     """Gera um nome completo formatado de modo elegante."""
 
-    full_name = first + ' ' + middle + ' ' + last
+    if middle:
+        full_name = first + ' ' + middle + ' ' + last
+
+    else:
+        full_name = first + ' ' + last
 
     return full_name.title()
