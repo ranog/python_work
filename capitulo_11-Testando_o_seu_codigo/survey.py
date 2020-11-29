39611c669b418a5e24e59fa6e4547bf9dce7c9f2
ranog <jprnogueira@yahoo.com.br>
2020-11-29 02:36:14 -0300

    capitulo_11-Testando_o_seu_codigo/survey.py

---
+++ capitulo_11-Testando_o_seu_codigo/survey.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+"""
+NOME
+    survey.py - Uma classe para testar.
+
+SINOPSES
+    
+------------------------------------------------------------------------
+
+DESCRIÇÃO
+
+------------------------------------------------------------------------
+
+HISTÓRICO
+    20202811: João Paulo, novembro de 2020.
+        - Uma classe para testar (pg 265-266).
+
+------------------------------------------------------------------------
+
+"""
+
+
+class AnonymousSurvey():
+    """Coleta respostas anônimas para uma pergunta de uma pesquisa."""
+
+    def __init__(self, question):
+
