351030263fbe0683867de33ba23c11fc317cb897
ranog <jprnogueira@yahoo.com.br>
2020-12-03 07:44:42 -0300

    Visualizando os repositórios usando o Pygal (pg 430-432).

--- capitulo_17-Trabalhando_com_APIs/python_repos.py
+++ capitulo_17-Trabalhando_com_APIs/python_repos.py
@@ -187,12 +187,17 @@ HISTÓRICO
 
     20200312: João Paulo, dezembro de 2020.
         - Resumo dos principais repositórios (pg 428-429).
+        - Visualizando os repositórios usando o Pygal (pg 430-432).
 
 ------------------------------------------------------------------------
 """
 
 
 import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 
 # Faz uma chamada de API e armazena a resposta.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -203,14 +208,22 @@ print("Status code:", r.status_code)
 # Armazena a resposta da API em uma variável
 response_dict = r.json()
 
+#-----------------------------------------------------------------------
+
 # Processa o resultado
 # print(response_dict.keys()) pg: 425-426
+
+#-----------------------------------------------------------------------
 print("Total repositories:", response_dict['total_count'])
 
 # Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
 
+#-----------------------------------------------------------------------
+
+# print("Repositories returned:", len(repo_dicts))
+
+#-----------------------------------------------------------------------
 
 """# Analisa o 1° repositório
 repo_dict = repo_dicts[0]
@@ -218,6 +231,9 @@ print("\nKeys =", len(repo_dict))
 for key in sorted(repo_dict.keys()):
     print(key)"""
 
+#-----------------------------------------------------------------------
+
+"""
 print("\nSelected information about each repository:")
 
 for repo_dict in repo_dicts:
@@ -228,4 +244,20 @@ for repo_dict in repo_dicts:
     print('Created:', repo_dict['created_at'])
     print('Updated:', repo_dict['updated_at'])
     print('Description:', repo_dict['description'])
-    print()
+    print()"""
+
+#-----------------------------------------------------------------------
+
+names, stars = [], []
+
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+    # Cria uma visualização
+    my_style = LS('#333366', base_style=LCS)
+    chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+    chart.title = 'Most-Starred Python Projects on GitHub'
+    chart.x_labels = names
+    chart.add('', stars) 
+    chart.render_to_file('python_repos.svg')
+
