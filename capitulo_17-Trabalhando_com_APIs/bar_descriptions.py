5a0c2dddcf06ccced2996ea94dd6b39e291b25d4
ranog <jprnogueira@yahoo.com.br>
2020-12-04 07:28:39 -0300

    Acrescentando dicas de contexto personalizadas (pg 434).

---
+++ capitulo_17-Trabalhando_com_APIs/bar_descriptions.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+
+"""
+------------------------------------------------------------------------
+
+NOME
+    bar_descriptions.py - Acrescentando dicas de contexto
+    personalizadas.
+
+------------------------------------------------------------------------
+
+SINOPSES
+    $ chmod +x bar_descriptions.py
+    $ ./bar_descriptions.py
+
+------------------------------------------------------------------------
+
+HISTÓRICO
+    20200412: João Paulo, dezembro de 2020.
+        - Acrescentando dicas de contexto personalizadas (pg 434).
+
+------------------------------------------------------------------------
+"""
+
+
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+
+
