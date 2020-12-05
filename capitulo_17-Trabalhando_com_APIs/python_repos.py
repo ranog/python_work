58b10450f611e8fb63a679d997faadcde34c50e8
ranog <jprnogueira@yahoo.com.br>
2020-12-04 23:27:25 -0300

    Plotando os dados (pg 435).

--- capitulo_17-Trabalhando_com_APIs/python_repos.py
+++ capitulo_17-Trabalhando_com_APIs/python_repos.py
@@ -229,6 +229,16 @@ DESCRIÇÃO
 
 ------------------------------------------------------------------------
 
+    Criamos uma lista vazia para names e outra para plot_dicts. Ainda
+    precisamos da lista names para gerar os rótulos do eixo x.
+    No laço criamos o dicionário plot_dict para cada projeto.
+    Armazenamos o número de estrelas com a chave 'value' e a descrição
+    do projeto com a chave 'label' em cada plot_dict. Então concatenamos
+    o plot_dict de cada projeto em plot_dicts. Passamos a lista
+    plot_dicts para add() .
+
+------------------------------------------------------------------------
+
 HISTÓRICO
     20200112: João Paulo, dezembro de 2020.
         - Processando uma resposta de AP (pg 425-426).
@@ -274,7 +284,7 @@ print("Total repositories:", response_dict['total_count'])
 
 # Explora informações sobre os repositórios
 repo_dicts = response_dict['items']
-print("Number of items:", len(repo_dicts))
+# print("Number of items:", len(repo_dicts))
 
 #-----------------------------------------------------------------------
 
@@ -305,25 +315,27 @@ names, plot_dicts = [], []
 
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-#    stars.append(repo_dict['stargazers_count']) - exemplo anterior
-    plot_dict = {'value' : repo_dict['stargazers_count'],
-            'label' : repo_dict['description'],}
+
+    plot_dict = {'value': repo_dict['stargazers_count'],
+                 'label': repo_dict['description'],}
     plot_dicts.append(plot_dict)
 
-    # Cria uma visualização
-    my_style = LS('#333366', base_style=LCS)
-    my_config = pygal.Config()
-    my_config.x_label_rotation = 45
-    my_config.show_legend = False
-    my_config.title_font_size = 24
-    my_config.label_font_size = 14
-    my_config.major_label_font_size = 18
-    my_config.truncate_label = 15
-    my_config.show_y_guides = False
-    my_config.width = 1000
+# Cria uma visualização
+my_style = LS('#333366', base_style=LCS)
 
-    chart = pygal.Bar(my_config, style=my_style)
-    chart.title = 'Most-Starred Python Projects on GitHub'
-    chart.x_labels = names
-    chart.add('', plot_dicts)
-    chart.render_to_file('python_repos.svg')
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
