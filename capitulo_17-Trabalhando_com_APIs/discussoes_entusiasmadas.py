5fbb7c122c2a6f9d2c1396dee6472d6740e6e6f8
ranog <jprnogueira@yahoo.com.br>
2020-12-08 07:19:02 -0300

    17.2 – Discussões entusiasmadas (pg 440).

---
+++ capitulo_17-Trabalhando_com_APIs/discussoes_entusiasmadas.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python3
+
+"""
+------------------------------------------------------------------------
+
+NOME
+    discussoes_entusiasmadas.py - FAÇA VOCÊ MESMO.
+
+------------------------------------------------------------------------
+
+SINOPSES
+    $ chmod +x discussoes_entusiasmadas.py
+    $ ./discussoes_entusiasmadas.py
+
+------------------------------------------------------------------------
+
+DESCRIÇÃO
+    17.2 – Discussões entusiasmadas: Usando os dados de
+    hn_submissions.py, crie um gráfico de barras que mostre as
+    discussões mais entusiasmadas do momento no Hacker News. A altura de
+    cada barra deve corresponder ao número de comentários que cada
+    artigo submetido tem. O rótulo de cada barra deve incluir o título
+    do artigo submetido, e cada barra deve atuar como um link para a
+    página de discussão desse artigo.
+
+------------------------------------------------------------------------
+
+HISTÓRICO
+    20200812: João Paulo, dezembro de 2020.
+        17.2 – Discussões entusiasmadas (pg 440).
+
+------------------------------------------------------------------------
+"""
+
+
+import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+
+# Faz uma chamada de API e armazena a resposta.
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+# Armazena a resposta da API em uma variável
+response_dict = r.json()
+
+#print("Total repositories:", response_dict['total_count'])
+
+# Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+# print("Number of items:", len(repo_dicts))
+
+names, plot_dicts = [], []
+
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+
+    plot_dict = {'value': repo_dict['stargazers_count'],
+                 'label': repo_dict['description'],
+                 'xlink': repo_dict['html_url'],}
+    plot_dicts.append(plot_dict)
+
+# Cria uma visualização
+my_style = LS('#333366', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
