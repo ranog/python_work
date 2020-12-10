b9980aa4d1a5b788510760eecd4e34ca954454d8
ranog <jprnogueira@yahoo.com.br>
2020-12-10 17:31:22 -0300

    17.3 – Testando python_repos.py (pg 440).

--- capitulo_17-Trabalhando_com_APIs/python_repos.py
+++ capitulo_17-Trabalhando_com_APIs/python_repos.py
@@ -281,10 +281,7 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 def chamada_api():
     # Faz uma chamada de API e armazena a resposta.
     url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
-
     r = requests.get(url)
-    #print("Status code:", r.status_code)
-
     return r
 
 # Armazena a resposta da API em uma variável
--- capitulo_17-Trabalhando_com_APIs/test_python_repos.py
+++ capitulo_17-Trabalhando_com_APIs/test_python_repos.py
@@ -7,7 +7,11 @@ NOME
 SINOPSES
     $ chmod +x test_python_repos.py
     $ ./test_python_repos.py
+    ...
+    --------------------------------------------------------------------
+    Ran 3 tests in 2.641s
 
+    OK
 ------------------------------------------------------------------------
 
 DESCRIÇÃO
@@ -25,7 +29,9 @@ HISTÓRICO
     20200912: João Paulo, dezembro de 2020.
         - 17.3 – Testando python_repos.py (pg 440).
 
-"""
+    20201012: João Paulo, dezembro de 2020.
+        - Refatoração do código.
+        - Implementação do método setUp()."""
 
 
 import unittest
@@ -35,28 +41,31 @@ from python_repos import chamada_api
 
 class PythonReposTestCase(unittest.TestCase):
     """Testes para 'python_repos.py'."""
+    
+
+    def setUp(self):
+        """Faz a chamada da API para todos os testes."""
+        self.r = chamada_api()
+        self.response_dict = self.r.json()
+
 
     def test_status_code(self):
         """O valor de status_code é 200?""" 
-        r = chamada_api()
-
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(self.r.status_code, 200)
 
 
     def test_num_repos(self):
         """O número total de repositórios é maior que 600000?."""
-        r = chamada_api()
-        response_dict = r.json()
-        
         # Testa se x >= y
-        self.assertGreaterEqual(int(response_dict['total_count']), 600000)
+        self.assertGreaterEqual(int(self.response_dict['total_count']), 600000)
 
 
     def test_num_itens(self):
         """o número de itens devolvidos é o que se espera?"""
-        r = chamada_api()
-        response_dict = r.json()
-        num_items = len(response_dict['items'])
+        num_items = len(self.response_dict['items'])
         
         self.assertGreaterEqual(num_items, 20)
         self.assertEqual(num_items, 30)
+
+
+unittest.main()
