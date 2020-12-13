116d2ebc52ab90b8986e7eb944ce963a36371f18
ranog <jprnogueira@yahoo.com.br>
2020-12-13 13:53:53 -0300

    Definindo o modelo Entry (pg 455-456).

--- capitulo_18-Introducao_ao_Django/learning_log/learning_logs/models.py
+++ capitulo_18-Introducao_ao_Django/learning_log/learning_logs/models.py
@@ -102,7 +102,8 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Algo específico aprendido sobre um assunto."""
 
-    topic = models.ForeignKey(Topic)
+    
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
