00f1dbdc9634c94d08a8f3352744fa9fb8201cb1
ranog <jprnogueira@yahoo.com.br>
2020-12-13 02:18:29 -0300

    Registrando um modelo junto ao site de administração (pg 453-454).

--- capitulo_18-Introducao_ao_Django/learning_log/learning_logs/admin.py
+++ capitulo_18-Introducao_ao_Django/learning_log/learning_logs/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+"""Esse código importa o modelo que queremos registrar, Topic, e então
+usa admin.site.register() para dizer a Django que administre nosso
+modelo por meio do site de administração."""
+
+from learning_logs.models import Topic
+
+admin.site.register(Topic)
