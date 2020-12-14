738b7b3875f2e7bca2b391feada94ebdb163fa0d
João Paulo Ramos Nogueira <jprnogueira@yahoo.com.br>
2020-12-14 09:19:47 -0300

    18.2 – Entradas menores (pg 459-460).

--- capitulo_18-Introducao_ao_Django/learning_log/learning_logs/models.py
+++ capitulo_18-Introducao_ao_Django/learning_log/learning_logs/models.py
@@ -120,15 +120,15 @@ class Entry(models.Model):
         """Devolve uma representação em string do modelo."""
 
 
-        """ 18.2 – Entradas menores (pg 459-460):
-            No momento, o método __str__() no modelo Entry concatena
-            reticências a todas as instâncias de Entry quando Django
-            exibe uma entrada no site de administração ou no shell.
-            Acrescente uma instrução if no método __str__() que adicione
-            reticências somente se a entrada tiver mais de 50
-            caracteres. Utilize o site de administração para acrescentar
-            uma entrada com menos de 50 caracteres e certifique-se de
-            que ela não contenha reticências quando for visualizada."""
+            # 18.2 – Entradas menores (pg 459-460):
+            # No momento, o método __str__() no modelo Entry concatena
+            # reticências a todas as instâncias de Entry quando Django
+            # exibe uma entrada no site de administração ou no shell.
+            # Acrescente uma instrução if no método __str__() que adicione
+            # reticências somente se a entrada tiver mais de 50
+            # caracteres. Utilize o site de administração para acrescentar
+            # uma entrada com menos de 50 caracteres e certifique-se de
+            # que ela não contenha reticências quando for visualizada.
 
         if len(self.text) > 50:
             return self.text[:50] + "..."
