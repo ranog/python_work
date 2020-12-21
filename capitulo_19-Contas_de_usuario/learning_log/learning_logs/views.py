2075d9243604c3ef9823cc5f92fa86130b868a13
ranog <jprnogueira@yahoo.com.br>
2020-12-21 13:31:01 -0300

    Função de view new_topic() (pg 479).

--- capitulo_19-Contas_de_usuario/learning_log/learning_logs/views.py
+++ capitulo_19-Contas_de_usuario/learning_log/learning_logs/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect 
+
+# Livro: from django.core.urlresolvers import reverse
+from django.urls import reverse
+
 from . models import Topic
+from . forms import TopicForm
 
 # Create your views here.
 """ Inicialmente importamos o modelo associado aos dados de que
@@ -43,7 +49,20 @@ def topic(request, topic_idid
     recentes serem exibidas antes.
 
     Armazenamos o assunto e as entradas no dicionário de contexto e
-    enviamos context para o template topic.html."""
+    enviamos context para o template topic.html.
+
+------------------------------------------------------------------------
+
+    Importamos a classe HttpResponseRedirect, que usaremos para
+    redirecionar o leitor de volta à página topics, depois que ele tiver
+    submetido o seu assunto.
+
+    A função reverse() determina o URL a partir de um padrão de URL
+    nomeado, o que quer dizer que Django gerará o URL quando a página
+    for solicitada.
+
+    Também importamos TopicForm, que é o formulário que acabamos de
+    criar. """
 
 
 def index(request):
@@ -64,3 +83,22 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+ 
+def new_topic(request):
+    """Adiciona um novo assunto."""
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulário em branco
+        form = TopicForm()
+
+    else:
+         # Dados de POST submetidos; processa os dados
+        form = TopicForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
