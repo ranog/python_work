4f4a85b6d1addcc21a8207737d21d53dcbc7a589
ranog <jprnogueira@yahoo.com.br>
2020-12-23 13:23:59 -0300

    ModelForm para entradas (pg 482-483).

--- capitulo_19-Contas_de_usuario/learning_log/learning_logs/forms.py
+++ capitulo_19-Contas_de_usuario/learning_log/learning_logs/forms.py
@@ -11,11 +11,38 @@ trabalharemos.
     campo text.
 
     O código labels = {'text': ''} diz a Django para não gerar um rótulo
-    para o campo text. """
+    para o campo text.
+
+------------------------------------------------------------------------
+
+    Inicialmente atualizamos a instrução import para incluir Entry, além
+    de Topic.
+
+    A nova classe EntryForm herda de forms.ModelForm e tem uma classe
+    Meta aninhada que lista o modelo no qual ela está baseada e o campo
+    a ser incluído no formulário.
+
+    Novamente especificamos um rótulo vazio para o campo 'text'.
+
+    Incluímos o atributo widgets.
+
+    Um widget é um elemento de formulário HTML, por exemplo, uma caixa
+    de texto de uma única linha, uma área de texto com várias linhas ou
+    uma lista suspensa.
+
+    Ao incluir o atributo widgets, podemos sobrescrever as opções
+    default de widgets de Django.
+
+    Ao dizer a Django para usar um elemento forms.Textarea, estamos
+    personalizando o widget de entrada para o campo 'text' de modo que a
+    área de texto tenha 80 colunas, em vez de usar o default de 40.
+
+    Isso dará espaço suficiente aos usuários para redigir uma entrada
+    significativa."""
 
 from django import forms
 
-from . models import Topic
+from . models import Topic, Entry
 
 
 class TopicForm(forms.ModelForm):
@@ -26,3 +53,10 @@ class TopicForm(forms.ModelForm):
         labels = {'text': ''}
 
 
+class EntryForm(forms.ModelForm):
+    """Criar um formulário associado ao modelo Entry."""
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
