f21923cc93d7f2bcc9b859e49bfbf93c8a346d4c
ranog <jprnogueira@yahoo.com.br>
2020-12-29 11:29:55 -0300

    Criando a classe Alien (pg 312).

---
+++ capitulo_13-Alienigenas/alien.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+"""
+------------------------------------------------------------------------
+
+NOME
+        alien.py - Criando a classe Alien
+
+------------------------------------------------------------------------
+
+SINOPSES
+        from alien import Alien
+
+------------------------------------------------------------------------
+
+DESCRIÇÃO
+        A maior parte dessa classe é semelhante à classe Ship, exceto
+        pelo posicionamento do alienígena.
+
+        Posicionaremos inicialmente cada alienígena próximo ao canto
+        superior esquerdo da tela, colocando um espaço à esquerda que
+        seja igual à largura do alienígena e um espaço acima dele
+        correspondente à sua altura.
+
+------------------------------------------------------------------------
+
+HISTÓRICO
+        20202912: João Paulo, dezembro de 2020.
+            - Criando a classe Alien (pg 312).
+
+------------------------------------------------------------------------
+"""
+
+
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Uma classe que representa um único alienígena da frota."""
+
+    def __init__(self, ai_settings, screen):
+        """Inicializa o alienígena e define sua posição inicial."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Carrega a imagem do alienígena e define seu atributo rect.
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Inicia cada novo alienígena próximo à parte superior esquerda
+        # da tela.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Armazena a posição exata do alienígena.
+        self.x = float(self.rect.x)
+
+
+    def blitme(self):
+        """Desenha o alienígena em sua posição atual."""
+        self.screen.blit(self.image, self.rect)
