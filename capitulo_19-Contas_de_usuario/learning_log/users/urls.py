207fabff206261e3d4cc78f4bfe28d0dfff0285a
ranog <jprnogueira@yahoo.com.br>
2021-01-20 06:32:29 -0300

    URL register (pg 495).

--- capitulo_19-Contas_de_usuario/learning_log/users/urls.py
+++ capitulo_19-Contas_de_usuario/learning_log/users/urls.py
@@ -5,16 +5,21 @@
 # No livro:
 # from django.conf.urls import url
 # from django.contrib.auth.views import login
+# from django.contrib.auth import login
+
 from django.urls import path
-#from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from . import views
 
 
 urlpatterns = [
     # Página de login.
-    path("login/", LoginView.as_view(template_name="users/login.html"),name="login"),
+    path("login/", LoginView.as_view(template_name="users/login.html"),
+        name='login'),
 
     # Página de logout.
-    path("logout/", views.logout_view, name='logout')
+    path("logout/", views.logout_view, name='logout'),
+
+    # Página de cadastro.
+    path("register/", views.register, name='register'),
 ]
