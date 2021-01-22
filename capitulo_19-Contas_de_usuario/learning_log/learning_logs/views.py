a692e3584725e0723492df11cb9bc5ab5b1c5933
ranog <jprnogueira@yahoo.com.br>
2021-01-22 07:09:50 -0300

    Restringindo o acesso em Learning Log (pg 499-500).

--- capitulo_19-Contas_de_usuario/learning_log/learning_logs/views.py
+++ capitulo_19-Contas_de_usuario/learning_log/learning_logs/views.py
@@ -228,6 +228,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Mostra um único assunto e todas as suas entradas."""
     topic = Topic.objects.get(id=topic_id)
@@ -236,6 +237,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Adiciona um novo assunto."""
     if request.method != 'POST':
@@ -253,6 +255,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Acrescenta uma nova entrada para um assunto em particular."""
     topic = Topic.objects.get(id=topic_id)
@@ -274,6 +277,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edita uma entrada existente."""
     entry = Entry.objects.get(id=entry_id)
