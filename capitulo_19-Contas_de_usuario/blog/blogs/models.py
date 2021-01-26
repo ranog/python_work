a9b0a5b862c030667fd3165df9a5ebf043922bc6
ranog <jprnogueira@yahoo.com.br>
2021-01-26 06:45:58 -0300

    19.5 – Blog protegido: Migrando o banco de dados (pg 502).

--- capitulo_19-Contas_de_usuario/blog/blogs/models.py
+++ capitulo_19-Contas_de_usuario/blog/blogs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
 
 
 class BlogPost(models.Model):
@@ -10,6 +11,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
