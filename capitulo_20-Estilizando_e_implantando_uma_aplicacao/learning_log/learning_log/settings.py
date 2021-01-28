9fc1abc5e89920770df8e97f0d22793458b384a3
ranog <jprnogueira@yahoo.com.br>
2021-01-28 06:31:16 -0300

    Aplicação django-bootstrap3 (pg 510).

--- capitulo_20-Estilizando_e_implantando_uma_aplicacao/learning_log/learning_log/settings.py
+++ capitulo_20-Estilizando_e_implantando_uma_aplicacao/learning_log/learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Aplicações de terceiros
+    'bootstrap3',
+
     # Minhas aplicações
     'learning_logs',
     'users',
@@ -125,3 +128,8 @@ STATIC_URL = '/static/'
 
 # Minhas configurações
 LOGIN_URL = '/users/login/'
+
+# Configurações para django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
