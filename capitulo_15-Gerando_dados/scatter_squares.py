66e92d58dbc150cb763effe9c7bb8f43ba278fbc
ranog <jprnogueira@yahoo.com.br>
2021-03-04 13:13:46 -0300

    Plotando uma série de pontos com scatter() (pg 373).

--- capitulo_15-Gerando_dados/scatter_squares.py
+++ capitulo_15-Gerando_dados/scatter_squares.py
@@ -12,11 +12,17 @@ HISTÓRICO
     20210303: João Paulo, março de 2021.
         - Plotando e estilizando pontos individuais com scatter()
         (pg 372).
+
+    20210403: João Paulo, março de 2021.
+        - Plotando uma série de pontos com scatter() (pg 373).
 """
 
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)
 
 # Define o título do gráfico e nomeia os eixos.
 plt.title("Square Numbers", fontsize=24)
