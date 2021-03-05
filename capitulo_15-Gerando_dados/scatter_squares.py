6e42d0694c912c43f481bfc2850e478e3289d8bd
ranog <jprnogueira@yahoo.com.br>
2021-03-05 13:20:46 -0300

    Calculando dados automaticamente (373-374).

--- capitulo_15-Gerando_dados/scatter_squares.py
+++ capitulo_15-Gerando_dados/scatter_squares.py
@@ -15,21 +15,27 @@ HISTÓRICO
 
     20210403: João Paulo, março de 2021.
         - Plotando uma série de pontos com scatter() (pg 373).
+
+    20210503: João Paulo, março de 2021.
+        - Calculando dados automaticamente (373-374).
 """
 
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, s=40)
 
-# Define o título do gráfico e nomeia os eixos.
+# Define o título do gráfico e nomeia os eixos:
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
-# Define o tamanho dos rótulos das marcações.
+# Define o tamanho dos rótulos das marcações:
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Define o intervalo para cada eixo:
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
