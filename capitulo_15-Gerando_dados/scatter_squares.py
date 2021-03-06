153d459ea23c51ce656a2fac842631aaa2fc7066
ranog <jprnogueira@yahoo.com.br>
2021-03-05 23:19:37 -0300

    Definindo cores personalizadas (pg 375).

--- capitulo_15-Gerando_dados/scatter_squares.py
+++ capitulo_15-Gerando_dados/scatter_squares.py
@@ -19,6 +19,7 @@ HISTÓRICO
     20210503: João Paulo, março de 2021.
         - Calculando dados automaticamente (pg 373-374);
         - Removendo os contornos dos pontos de dados (pg 374).
+        - Definindo cores personalizadas (pg 375).
 """
 
 import matplotlib.pyplot as plt
@@ -26,7 +27,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolor='none', s=40)
+plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
 
 # Define o título do gráfico e nomeia os eixos:
 plt.title("Square Numbers", fontsize=24)
