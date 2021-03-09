9bce0e2bba778cd2762ac24dda3b2ae07331ad0e
ranog <jprnogueira@yahoo.com.br>
2021-03-09 13:24:43 -0300

    Escolhendo as direções (pg 378).

--- capitulo_15-Gerando_dados/random_walk.py
+++ capitulo_15-Gerando_dados/random_walk.py
@@ -3,7 +3,10 @@
 """
 HISTÓRICO
     20210803: João Paulo, março de 2021.
-        - Criando a classe RandomWalk() (pg 377).
+        - Criando a classe RandomWalk() (pg 377);
+
+    20210903: João Paulo, março de 2021.
+        - Escolhendo as direções (pg 378).
 """
 
 from random import choice
@@ -23,3 +26,32 @@ class RandomWalk():
         # Todos os passeios começam em (0, 0):
         self.x_values = [0]
         self.y_values = [0]
+
+
+    def fill_walk(self):
+        """
+            Calcula todos os pontos do passeio.
+        """
+        # Continua dando passos até que o passeio alcance o tamanho
+        # desejado:
+        while len(self.x_values) < self.num_points:
+            # Decide direção a ser seguida e distância a ser percorrida
+            # nessa direção:
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # Rejeita movimentos que não vão a lugar nenhum
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calcula os próximos valores de x e de y:
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
