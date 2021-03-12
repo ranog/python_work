e4992f803287d6f14cb7306f184a00cb9beb3045
ranog <jprnogueira@yahoo.com.br>
2021-03-12 13:27:13 -0300

    Plotando os pontos de início e de fim (pg 381-382).

--- capitulo_15-Gerando_dados/rw_visual.py
+++ capitulo_15-Gerando_dados/rw_visual.py
@@ -27,7 +27,7 @@ while True:
 
     # Enfatiza o primeiro e o último ponto:
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     # Salva os gráficos automaautomatic:
     plt.savefig('random_walk_plot' + str(i) + '.png')
