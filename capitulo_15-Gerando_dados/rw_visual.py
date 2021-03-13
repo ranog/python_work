c776dd54544b90d92c8aea158e4907ec9c9b2dba
ranog <jprnogueira@yahoo.com.br>
2021-03-13 12:44:57 -0300

    Limpando os eixos (pg 382).

--- capitulo_15-Gerando_dados/rw_visual.py
+++ capitulo_15-Gerando_dados/rw_visual.py
@@ -7,6 +7,9 @@ HISTÓRICO
 
     20211203: João Paulo, março de 2021.
         - Plotando os pontos de início e de fim (pg 381-382).
+
+    20211303: João Paulo, março de 2021.
+        - Limpando os eixos (pg 382).
 """
 
 import matplotlib.pyplot as plt
@@ -23,14 +26,20 @@ while True:
 
     point_numbers = list(range(rw.num_points))
 
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15) 
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+            edgecolor='none', s=15)
 
     # Enfatiza o primeiro e o último ponto:
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+            s=100)
+
+    # Remove os eixos:
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
 
     # Salva os gráficos automaautomatic:
-    plt.savefig('random_walk_plot' + str(i) + '.png')
+    plt.savefig('random_walk_plot_sem_eixos' + str(i) + '.png')
     i += 1
 
     plt.show()
