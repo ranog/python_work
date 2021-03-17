65554ce43a6b063b9feec93355b2bdb2b004e878
ranog <jprnogueira@yahoo.com.br>
2021-03-17 13:25:02 -0300

    Criando a classe Die (pg 385-386).

---
+++ capitulo_15-Gerando_dados/die.py
@@ -0,0 +1,27 @@
+"""
+HOSTÓRICO
+    20211703: João Paulo, março de 2021.
+        Criando a classe Die (pg 385-386).
+"""
+
+from random import randint
+
+
+class Die():
+    """
+        Uma classe que representa um único dado.
+    """
+
+    
+    def __init__(self, num_sides=6):
+        """
+            Supõe que seja um dado de seis lados.
+        """
+        self.num_sides = num_sides
+
+
+    def roll(self):
+        """"
+            Devolve um valor aleatório entre 1 e o número de lados.
+        """
+        return randint(1, self.num_sides)
