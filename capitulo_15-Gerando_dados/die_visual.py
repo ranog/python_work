433e905d2318b17621715d24e8ccd463af6ee40c
ranog <jprnogueira@yahoo.com.br>
2021-03-19 13:20:29 -0300

    Analisando os resultados (pg 387).

--- capitulo_15-Gerando_dados/die_visual.py
+++ capitulo_15-Gerando_dados/die_visual.py
@@ -8,8 +8,16 @@ die = Die()
 # Faz alguns lançamentos e armazena os resultados em uma lista:
 results = []
 
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
-    
+
+# # Analisa os resultados:
+frequencies = []
+
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
 print(results)
