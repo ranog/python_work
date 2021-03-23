a42079d3a11464a6ee6614f69edbc8655d6031d8
ranog <jprnogueira@yahoo.com.br>
2021-03-23 13:23:43 -0300

    15.6 – Rótulos automáticos (pg 392).

--- capitulo_15-Gerando_dados/dice_visual.py
+++ capitulo_15-Gerando_dados/dice_visual.py
@@ -26,7 +26,7 @@ for value in range(2, max_result + 1):
 # Visualiza os resultados:
 hist = pygal.Bar()
 hist.title = "Results of rolling two D6 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_labels = [str(x) for x in range(2,13)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 hist.add('D6 + D6', frequencies)
--- capitulo_15-Gerando_dados/die_visual.py
+++ capitulo_15-Gerando_dados/die_visual.py
@@ -24,7 +24,7 @@ for value in range(1, die.num_sides + 1):
 # Visualiza os resultados:
 hist = pygal.Bar()
 hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels = [str(x) for x in range(1, 7)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 hist.add('D6', frequencies)
