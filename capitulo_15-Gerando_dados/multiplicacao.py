a2044614411e6bb7c865e8f1ef5945f55e99614d
ranog <jprnogueira@yahoo.com.br>
2021-03-26 13:55:29 -0300

    15.9 – Multiplicação (pg 392).

---
+++ capitulo_15-Gerando_dados/multiplicacao.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import pygal
+from die import Die
+
+
+# Cria dois dados D6:
+die_1 = Die()
+die_2 = Die()
+
+# Faz alguns lançamentos e armazena os resultados em uma lista:
+results = []
+
+for roll_num in range(1000):
+    result = die_1.roll() * die_2.roll()
+    results.append(result)
+
+# Analisa os resultados:
+frequencies = []
+max_result = die_1.num_sides * die_2.num_sides
+
+for value in range(1, max_result + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualiza os resultados:
+hist = pygal.Bar()
+hist.title = "Results of rolling and multiplying two D6 1000 times."
+hist.x_labels = [str(x_label) for x_label in range(1,37)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+hist.add('D6 * D6', frequencies)
+hist.render_to_file('multiplicacao_visual.svg')
