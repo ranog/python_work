a063a59e1aba2a33a4b712d05301267e57eecaca
ranog <jprnogueira@yahoo.com.br>
2021-03-28 23:46:01 -0300

    Fazendo parse dos cabeçalhos de arquivos CSV (pg 395-396).

---
+++ capitulo_16-Fazendo_download_de_dados/highs_lows.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import csv
+
+filename = 'sitka_weather_07-2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+print(header_row)
