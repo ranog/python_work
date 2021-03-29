b63cdfc5a318f852249363b105386a93ae8f89d1
ranog <jprnogueira@yahoo.com.br>
2021-03-29 12:16:50 -0300

    Exibindo os cabeçalhos e suas posições (pg 396-397).

--- capitulo_16-Fazendo_download_de_dados/highs_lows.py
+++ capitulo_16-Fazendo_download_de_dados/highs_lows.py
@@ -8,4 +8,5 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-print(header_row)
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
