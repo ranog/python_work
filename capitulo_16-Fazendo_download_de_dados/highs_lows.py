80979ce7ec89b9ec20e8fdacd204fab8721292f7
ranog <jprnogueira@yahoo.com.br>
2021-03-30 13:07:18 -0300

    Extraindo e lendo dados (pg 397-398).

--- capitulo_16-Fazendo_download_de_dados/highs_lows.py
+++ capitulo_16-Fazendo_download_de_dados/highs_lows.py
@@ -2,11 +2,17 @@
 
 import csv
 
+#Obtém as temperaturas máximas do arquivo:
 filename = 'sitka_weather_07-2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+    highs = []
+
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+print(highs)
