15dfbacecc83b80c5cf9cd234d9f18685df8bc7e
ranog <jprnogueira@yahoo.com.br>
2021-03-31 12:32:32 -0300

    Plotando dados em um gráfico de temperatura (pg 398-399).

--- capitulo_16-Fazendo_download_de_dados/highs_lows.py
+++ capitulo_16-Fazendo_download_de_dados/highs_lows.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import csv
+from matplotlib import pyplot as plt
+
 
 #Obtém as temperaturas máximas do arquivo:
 filename = 'sitka_weather_07-2014.csv'
@@ -15,4 +17,13 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-print(highs)
+# Faz a plotagem dos dados:
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# Formata o gráfico
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel("", fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
