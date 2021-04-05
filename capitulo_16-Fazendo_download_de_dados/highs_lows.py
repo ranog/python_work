4f513ca7b6d5c4b5216ac4ca9ecc7ad42efcb991
ranog <jprnogueira@yahoo.com.br>
2021-04-05 15:44:17 -0300

    Verificação de erros (pg 404-406).

--- capitulo_16-Fazendo_download_de_dados/highs_lows.py
+++ capitulo_16-Fazendo_download_de_dados/highs_lows.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 
 #Obtém as temperaturas máximas do arquivo:
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 with open(filename) as f:
     reader = csv.reader(f)
@@ -15,14 +15,16 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
 
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Faz a plotagem dos dados:
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -31,7 +33,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='grey', alpha=0.1)
 
 # Formata o gráfico
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA",
+        fontsize=20)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
