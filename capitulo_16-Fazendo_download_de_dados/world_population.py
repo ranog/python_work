b116c7b8430f66cb0c251a3939362104003fbfd3
ranog <jprnogueira@yahoo.com.br>
2021-04-06 21:30:27 -0300

    Extraindo dados relevantes (pg 407-409).

---
+++ capitulo_16-Fazendo_download_de_dados/world_population.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import json
+
+# Carrega os dados em uma lista:
+filename = 'population_data.json'
+
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Exibe a população de cada país em 2010:
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+
+        print(country_name + ": " + population)
