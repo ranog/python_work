f7d0044433295643ab580601f8ae8e33f55c524f
ranog <jprnogueira@yahoo.com.br>
2021-04-08 21:17:58 -0300

    Obtendo os códigos de duas letras dos países (pg 410-412).

--- capitulo_16-Fazendo_download_de_dados/countries.py
+++ capitulo_16-Fazendo_download_de_dados/countries.py
@@ -1,5 +1,8 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
-from pygal.i18n import COUNTRIES
+from pygal.maps.world import COUNTRIES
 
-
+# coloca as chaves em ordem alfabéfica.
+for country_code in sorted(COUNTRIES.keys()):
+    # exibe o código de cada país e o nome associado. 
+    print(country_code, COUNTRIES[country_code])
--- capitulo_16-Fazendo_download_de_dados/world_population.py
+++ capitulo_16-Fazendo_download_de_dados/world_population.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+from country_codes import get_country_code
 
 # Carrega os dados em uma lista:
 filename = 'population_data.json'
@@ -13,5 +14,9 @@ for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
+        code = get_country_code(country_name)
 
-        print(country_name + ": " + str(population))
+        if code:
+            print(code + ": " + str(population))
+        else:
+            print("ERROR - " + country_name)
