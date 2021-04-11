5d48f8c11982abb10fe27449c9ddc8e930b14e63
ranog <jprnogueira@yahoo.com.br>
2021-04-11 16:27:59 -0300

    Criando um mapa completo de populações (pg 414-416).

--- capitulo_16-Fazendo_download_de_dados/world_population.py
+++ capitulo_16-Fazendo_download_de_dados/world_population.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 import json
+import pygal
 from country_codes import get_country_code
 
-# Carrega os dados em uma lista:
+# Carrega os dados em uma lista.
 filename = 'population_data.json'
 
 with open(filename) as f:
     pop_data = json.load(f)
 
-# Exibe a população de cada país em 2010:
+# Constrói um dicionário com dados das populações.
+cc_populations = {}
+
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
-        country_name = pop_dict['Country Name']
+        country = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        code = get_country_code(country_name)
+        code = get_country_code(country)
 
         if code:
-            print(code + ": " + str(population))
-        else:
-            print("ERROR - " + country_name)
+            cc_populations[code] = population
+            wm = pygal.maps.world.World()
+            wm.title = 'World Population in 2010, by Country'
+            wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
